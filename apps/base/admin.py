755f2ca1f05eeff23e10c532645f6fa92070f371
isko18 <toksonbaevislam2004@gmail.com>
2024-06-29 19:42:13 +0600

    lesson_4.py

---
+++ apps/base/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from apps.base.models import Base, Popular_category, Our_chef
+# Register your models here.
+
+admin.site.register(Base)
+admin.site.register(Popular_category)
+admin.site.register(Our_chef)
+
+
+
+    
